ad0fbccebb0cab8d83f8018758a0565c17284ef1
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-20 19:53:24 +0200

    cart && products

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <?php
+
+session_start();
+
 require_once('./php/CreateDb.php');
 require_once('./php/component.php');
 
 // create instance of Createdb class
-
 $database = new CreateDb("Productdb", "Producttb");
 
+if (isset($_POST['add'])){
+    // print_r($_POST['product_id']);
+    if(isset($_SESSION['cart'])){
+
+        $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if(in_array($_POST['product_id'], $item_array_id)){
+            echo "<script>alert('Product is already added in the cart..!')</script>";
+            echo "<script>window.location = 'index.php'</script>";
+        }else{
+
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id' => $_POST['product_id']
+            );
+
+            $_SESSION['cart'][$count] = $item_array;
+        }
+
+    }else{
+
+        $item_array = array(
+                'product_id' => $_POST['product_id']
+        );
+
+        // Create new session variable
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,15 +52,7 @@ $database = new CreateDb("Productdb", "Producttb");
     <title>E-shop</title>
 </head>
 <body>
-    <div class="bg-secondary ">
-    <header class="container d-flex justify-content-between">
-    <h1 >E-Shop</h1>
-    <div class="d-flex mr-3">
-    <h1 >Login</h1>
-    <h1>Sign Up</h1>
-    </div>
-</header>
-</div>
+        <?php require_once("php/header.php")?>
 <main class="container d-flex direction-column text-center py-5">
     
     <div class="row text-center ">
