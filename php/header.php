6a20bc3ca9a9efa3a319e21d071aa7c916868240
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-23 01:57:16 +0200

    validation register form

--- php/header.php
+++ php/header.php
@@ -20,12 +20,12 @@
 
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="mr-auto"></div>
-            <div class="navbar-nav ">
-                <a href="cart.php" class="nav-item nav-link active">
-                    <h5 class="d-flex px-5 cart">
+            <div class="navbar-nav align-items-center h4 text-primary">
                       
                         <?php
                              if(isset($_SESSION['admin_name']) || isset($_SESSION['user_name'])){
+                                echo '<a href="cart.php" class="nav-item nav-link active">';
+                                echo '<h5 class="d-flex align-items-center px-5 cart">';
                                 echo "<i class=\"fas fa-shopping-cart\"></i> Cart";
                                 if (isset($_SESSION['cart'])){
                                     $count = count($_SESSION['cart']);
@@ -33,16 +33,18 @@
                                 }else{
                                     echo "<span id=\"cart_count\" class=\"text-warning bg-light rounded-circle p-2 \">0</span>";
                                 }
+                                echo '</h5>';
+                                echo '</a>';
                              }
                         ?>
                          <?php
            if (isset($_SESSION['admin_name'])) {
             // Prijavljen je admin
-            echo '<li>Dobrodošli, ' . $_SESSION['admin_name'] . '!</li>';
+            echo '<li class="mr-3">Dobrodošli, ' . $_SESSION['admin_name'] . '!</li>';
             echo '<li><a href="logout.php">Odjava</a></li>';
         } elseif (isset($_SESSION['user_name'])) {
             // Prijavljen je korisnik
-            echo '<li >Dobrodošli, ' . $_SESSION['user_name'] . '!</li>';
+            echo '<li class="mr-3">Dobrodošli, ' . $_SESSION['user_name'] . '!</li>';
             echo '<li><a href="logout.php">Odjava</a></li>';
         } else {
             // Korisnik nije prijavljen
