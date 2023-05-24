e5331cc6a3700594a81cd808f341b403ffc016ec
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-24 17:33:54 +0200

    Delete product && style table

--- admin.php
+++ admin.php
@@ -7,7 +7,6 @@
 // }
 
 
-
 if(isset($_POST['add_product'])){
     $p_name = $_POST['p_name'];
     $p_description = $_POST['p_description'];
@@ -21,11 +20,26 @@ if(isset($_POST['add_product'])){
     if($insert_query){
         move_uploaded_file($p_image_tmp_name, $p_image_folder);
         $message[] = 'Product add succesfully';
+        // header('location:admin.php');
     }else{
         $message[] = 'could not add the product';
 
     }
 }
+
+if(isset($_GET['delete'])){
+    $delete_id = $_GET['delete'];
+    $delete_query = mysqli_query($product_db, "DELETE FROM `products` WHERE id = $delete_id");
+    if($delete_query){
+        header('location:admin.php');
+        $message[] = 'Product deleted';
+    }else{
+        header('location:admin.php');
+        $message[] = 'Product could not be deleted';
+    }
+};
+
+
 
 ?>
 <!DOCTYPE html>
@@ -42,15 +56,17 @@ if(isset($_POST['add_product'])){
 <body>
     
 <?php
+
 if(isset($message)){
-    foreach($message as $message){
-        echo '<div class="product-msg"><span>'.$message.'</span><i class="fa-solid fa-x" onclick="this.parentElement.style.display = `none`;"></i></div>';
-    };
+   foreach($message as $message){
+      echo '<div class="product_msg"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+   };
 };
+
 ?>
 <?php require_once("php/header.php")?>
-<div class="container mt-5 bg-secondary w-50">
-<section >
+<div class="container mt-5">
+<section class="bg-secondary p-3 mb-5" >
     <form action="" method="post" class="add-product-form" enctype="multipart/form-data">
     <h3 class="text-white pt-3">Add a new product</h3>
     <input type="text" name="p_name" placeholder="Enter the product name" class="w-100 mt-3" required>
@@ -60,7 +76,50 @@ if(isset($message)){
     <input type="submit" value="Add the product" name="add_product" class="btn btn-primary w-100 mb-3 mt-3">
     </form>
 </section>
+
+<section class="display-product-table">
+
+<table>
+
+<thead>
+   <th>Product Image</th>
+   <th>Product Name</th>
+   <th>Product Price</th>
+   <th>Action</th>
+</thead>
+
+<tbody>
+   <?php
+   
+      $select_products = mysqli_query($product_db, "SELECT * FROM `products`");
+      if(mysqli_num_rows($select_products) > 0){
+         while($row = mysqli_fetch_assoc($select_products)){
+   ?>
+
+   <tr>
+      <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
+      <td><?php echo $row['name']; ?></td>
+      <td>$<?php echo $row['price']; ?>/-</td>
+      <td>
+         <a href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn" onclick="return confirm('are your sure you want to delete this?');"> <i class="fas fa-trash"></i> delete </a>
+      </td>
+   </tr>
+
+   <?php
+      };    
+      }else{
+         echo "<div class='empty'>no product added</div>";
+      };
+   ?>
+</tbody>
+</table>
+
+
+</section>
+
 </div>
+
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
