f4e36df9335498ee5ec39e14ab5bba11a4c483d4
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-25 19:46:46 +0200

    checkout form order

---
+++ checkout.php
@@ -0,0 +1,118 @@
+<?php
+session_start();
+@include 'config.php';
+require_once ("php/CreateDb.php");
+$db = new CreateDb("Productdb", "Producttb");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Checkout</title>
+    <link rel="stylesheet" href="./css/checkout.css">
+</head>
+<body>
+<?php
+    require_once ('php/header.php');
+?>
+<div class="container">
+
+<section class="checkout-form">
+
+
+
+   <h1 class="heading">complete your order</h1>
+   <div class="display-order">
+   <?php
+
+$total = 0;
+    if (isset($_SESSION['cart'])){
+        $product_id = array_column($_SESSION['cart'], 'product_id');
+
+        $result = $db->getData();
+        while ($row = mysqli_fetch_assoc($result)){
+            foreach ($product_id as $id){
+                if ($row['id'] == $id){
+                  $product_name = $row['product_name'];
+                  echo "<h5>$product_name</h5>";
+                  //   cartElement($row['product_image'], $row['product_name'],$row['product_price'], $row['id']);
+                    $total = $total + (int)$row['product_price'];
+                }
+            }
+        }
+    }else{
+        echo "<h5>Cart is Empty</h5>";
+    }
+
+?>
+
+   </div>
+
+
+   <form action="" method="post">
+
+  
+      <div class="flex">
+         <div class="inputBox">
+            <span>Your name</span>
+            <input type="text" placeholder="enter your name" name="name" required>
+         </div>
+         <div class="inputBox">
+            <span>Your number</span>
+            <input type="number" placeholder="enter your number" name="number" required>
+         </div>
+         <div class="inputBox">
+            <span>Your email</span>
+            <input type="email" placeholder="enter your email" name="email" required>
+         </div>
+         <div class="inputBox">
+            <span>Payment method</span>
+            <select name="method">
+               <option value="cash on delivery" selected>cash on devlivery</option>
+               <option value="credit cart">credit cart</option>
+               <option value="paypal">paypal</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>Address line 1</span>
+            <input type="text" placeholder="e.g. flat no." name="flat" required>
+         </div>
+         <div class="inputBox">
+            <span>Address line 2</span>
+            <input type="text" placeholder="e.g. street name" name="street" required>
+         </div>
+         <div class="inputBox">
+            <span>City</span>
+            <input type="text" placeholder="e.g. Novi Pazar" name="city" required>
+         </div>
+         <div class="inputBox">
+            <span>State</span>
+            <input type="text" placeholder="e.g. ras" name="state" required>
+         </div>
+         <div class="inputBox">
+            <span>Country</span>
+            <input type="text" placeholder="e.g. Serbia" name="country" required>
+         </div>
+         <div class="inputBox">
+            <span>Pin code</span>
+            <input type="text" placeholder="e.g. 123456" name="pin_code" required>
+         </div>
+      </div>
+      <input type="submit" value="Order Now" name="order_btn" class="btn btn-primary w-100 p-3">
+   </form>
+
+</section>
+
+</div>
+<?php require_once("php/footer.php")?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
