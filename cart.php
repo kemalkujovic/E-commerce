315b40980b2412dafc3e67d6a95bbb635eeae9c4
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-27 17:08:16 +0200

    protect header && cart

--- cart.php
+++ cart.php
@@ -38,7 +38,7 @@ if (isset($_POST['remove'])){
     <!-- Font Awesome -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script defer src="js/script.js"></script>
--- php/header.php
+++ php/header.php
@@ -18,6 +18,11 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
+        
+
+
+
+        
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="mr-auto"></div>
             <div class="navbar-nav align-items-center h4 text-primary">
@@ -25,34 +30,51 @@
                         <?php
                              if(isset($_SESSION['admin_name']) || isset($_SESSION['user_name'])){
                                 echo '<a href="cart.php" class="nav-item nav-link active">';
-                                echo '<h5 class="d-flex align-items-center px-2 cart">';
+                                echo '<h5 class="d-flex align-items-center px-2 m-0 cart">';
                                 echo "<i class=\"fas fa-shopping-cart\"></i> Cart";
                                 if (isset($_SESSION['cart'])){
                                     $count = count($_SESSION['cart']);
-                                    echo "<span id=\"cart_count\" class=\"text-warning bg-light rounded-circle p-2\">$count</span>";
+                                    echo "<span id=\"cart_count\" class=\"text-warning bg-light rounded-circle ml-1 \">$count</span>";
                                 }else{
-                                    echo "<span id=\"cart_count\" class=\"text-warning bg-light rounded-circle p-2 \">0</span>";
+                                    echo "<span id=\"cart_count\" class=\"text-warning bg-light rounded-circle ml-1  \">0</span>";
                                 }
                                 echo '</h5>';
                                 echo '</a>';
                              }
                         ?>
+                        
+                        
                          <?php
+
            if (isset($_SESSION['admin_name'])) {
             // Prijavljen je admin
             echo '<li class="mr-3 text-white">Dobrodošli, ' . $_SESSION['admin_name'] . '!</li>';
-            echo '<li class="text-white"><a class="text-white" href="logout.php">Odjava</a></li>';
-            echo '<li class="ml-4 text-white"><a  href="admin.php"><i class="fa fa-user text-white"></i></a></li>';
         } elseif (isset($_SESSION['user_name'])) {
             // Prijavljen je korisnik
             echo '<li class="mr-3 text-white">Dobrodošli, ' . $_SESSION['user_name'] . '!</li>';
-            echo '<li><a class="text-white" href="logout.php">Odjava</a></li>';
         } else {
             // Korisnik nije prijavljen
             echo '<li class="d-flex mr-2 text-white"><a class="text-white btn btn-lg btn-outline-secondary" href="login.php">Prijava</a></li>';
             echo '<li><a class="text-white btn btn-lg btn-outline-secondary" href="registration.php">Registracija</a></li>';
         }
         ?>
+    <?php
+    if(isset($_SESSION['admin_name']) || isset($_SESSION['user_name'])){
+       echo  '<div class="dropdown dropleft">';
+        echo '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+        echo '<i class="fa fa-gear"></i>';
+        echo '</button>';
+        echo '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+       echo "<a class='dropdown-item text-center' href=logout.php>Logout <i class='fa fa-sign-out' aria-hidden='true'></i></a>";
+    }
+    if(isset($_SESSION['admin_name'])){
+        echo '<li><a class="dropdown-item text-center" href="admin.php">Admin <i class="fa fa-user"></i></a></li>';
+    }
+    
+    ?>
+
+</div>
+</div>
                     </h5>
                 </a>
             </div>
