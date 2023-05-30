e04e62362949f76a6ad3abf6e136405211c9f5d0
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-05-30 05:23:53 +0200

    style footer

--- php/footer.php
+++ php/footer.php
@@ -90,7 +90,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
       © 2021 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">Kemal Kujovic</a>
+      <a class="text-white" href="https://mdbootstrap.com/">Amina Salihovic</a>
     </div>
     <!-- Copyright -->
   </footer>
